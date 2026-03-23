cd1d5d7dd737458321acd1bc4f5c8df094baa325
Kshitij Kumar Jha <kshitijrsjha@gmail.com>
2026-03-23 18:01:20 +0530

    Time: 0 ms (100%), Space: 8.8 MB (67.06%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n = s.length();
+        int len = 0;
+    
+        for(int i = n - 1; i >= 0; i--) {
+            if(s[i] == ' ') {
+                if(len > 0) break; 
+            } else {
+                len++;
+            }
+        }
+        return len;
+    }
+};
